6af05895034e0248fb351b9b7c35a908f5da9b6a
duckchoon <skywolf321@naver.com>
2024-02-05 15:49:24 +0900

    Initial Commit

---
+++ thebeans/thebeansApp.swift
@@ -0,0 +1,17 @@
+//
+//  thebeansApp.swift
+//  thebeans
+//
+//  Created by OCUBE on 2024/02/05.
+//
+
+import SwiftUI
+
+@main
+struct thebeansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
